08f4e884d7b692bc66bb1cc21b768854f0d9ed50
Johannes Klaushofer <johannes.klaushofer@inode.at>
2016-01-18 09:51:07 +0100

    setup

---
+++ htdocs/wp-content/themes/modolab/functions.php
@@ -0,0 +1,33 @@
+<?php
+	// customize ACF path
+	function acf_settings_path($path){
+		return get_template_directory().'/plugins/advanced-custom-fields-pro/';
+	}
+	add_filter('acf/settings/path', 'acf_settings_path');
+	
+	// customize ACF dir
+	function acf_settings_dir($dir){
+		return get_template_directory_uri().'/plugins/advanced-custom-fields-pro/';
+	}
+	add_filter('acf/settings/dir', 'acf_settings_dir');
+	
+	// hide ACF field group menu item
+	add_filter('acf/settings/show_admin', '__return_false');
+	
+	// include ACF
+	include_once(get_stylesheet_directory().'/plugins/advanced-custom-fields-pro/acf.php');
+	
+	// activate theme
+	function mytheme_setup_options(){
+		if(function_exists('acf_add_local_field_group')){
+			$_FILES['acf_import_file'] = array(
+				'name' => 'fields.json',
+				'tmp_name' => get_template_directory().'/fields.json'
+			);
+			include_once(get_stylesheet_directory().'/plugins/advanced-custom-fields-pro/admin/settings-tools.php');
+			$acfSettings = new acf_settings_tools();
+			$acfSettings->import();
+		}
+	}
+	add_action('after_switch_theme', 'mytheme_setup_options');
+?>
---
+++ htdocs/wp-content/themes/modolab/readme.txt
@@ -0,0 +1,7 @@
+=== Modolab ===
+Contributors: Johannes Klaushofer
+Requires at least: WordPress 4.4
+Tested up to: WordPress 4.4
+Version: 0.1
+Tags: responsive, custom-layout, responsive-layout, custom, editor-style, flexible-content, translation-ready
+Description: Modolab is a full responsive and flexible content/layout theme word wordpress. Easy to use for everyone. Great for developers who want a powerful and total flexiible theme for their customers
